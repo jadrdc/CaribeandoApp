0422767ca436f3bd8bd80c24e62a2a2234bdbd86
Jose Agustin Reinoso <jadrdc@gmail.com>
2025-06-05 17:51:20 -0400

    Adding logger

--- composeApp/src/commonMain/kotlin/com/agusteam/caribeando/caribeando/presenter/Util.kt
+++ composeApp/src/commonMain/kotlin/com/agusteam/caribeando/caribeando/presenter/Util.kt
@@ -159,6 +159,5 @@ fun formatDouble(value: Double, decimals: Int = 1): String {
 }
 
 
-const val URL = "http://127.0.0.1:8081/"
-const val URL1 = "http://10.0.2.2:8081/"
+const val URL = "https://caribeando-backend-production.up.railway.app/"
 
